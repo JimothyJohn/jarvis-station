58e2003a1d887db50c5ed5d64283cb389c0c9de8
JimothyJohn <drunknmunky32@gmail.com>
2023-08-06 18:38:01 -0500

    SDH updated

--- jarvis/__main__.py
+++ jarvis/__main__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 from jlclient import jarvisclient
 from jlclient.jarvisclient import *
@@ -15,7 +16,7 @@ parser.add_argument(
     type=str,
     help="The type of NVIDIA GPU",
     choices=["RTX5000", "A100"],
-    default="RTX5000",
+    default="A100",
 )
 parser.add_argument(
     "--num_gpus",
@@ -27,20 +28,22 @@ parser.add_argument(
 parser.add_argument(
     "--hdd", type=int, help="The hard disk drive capacity (multiples of 10)", default=50
 )
-parser.add_argument("--name", type=str, help="The name of the instance", default="Dev")
+parser.add_argument("--name", help="The name of the instance", default="Dev")
 parser.add_argument(
-    "--is_reserved", type=bool, help="Reserve instance or spot instance", default=False
+    "--is_spot",
+    help="Do not reserve instance",
+    default=True,
+    action="store_false",
 )
 # This argument lets the user choose to destroy the instance
 parser.add_argument(
     "--destroy",
-    type=bool,
     help="Destroy the instance rather than create it",
-    default=False,
+    action="store_true",
 )
 # This argument lets the user input a file path for a setup script
 parser.add_argument(
-    "--script",
+    "--script_path",
     type=str,
     help="The path to the setup script",
     default="utils/LatentBlending.sh",
@@ -54,27 +57,33 @@ load_dotenv()  # take environment variables from .env.
 jarvisclient.token = os.environ.get(f"JARVISLABS_API_KEY")
 jarvisclient.user_id = os.environ.get(f"JARVISLABS_USER")
 
+if jarvisclient.token == None or jarvisclient.user_id == None:
+    print("Please set JARVISLABS_API_KEY and JARVISLABS_USER environment variables!")
+    exit()
+
 if __name__ == "__main__":
-    print(f"{User.get_script()}")
+    scripts = User.get_script()
     script_id = ""
-    if User.get_script()["success"] != False:
-        for script in User.get_script()["script_meta"]:
-            if script["script_name"] == args.script:
+    script_name = args.script_path.split("utils/")[1].split(".sh")[0]
+
+    if scripts["success"] != False:
+        for script in scripts["script_meta"]:
+            if script["script_name"] == script_name:
                 script_id = script["script_id"]
                 print("Script already exists!")
                 break
     else:
         print("Creating script...")
         script_id = User.add_script(
-            script_path=args.script,
-            script_name=args.script.split("utils/")[1].split(".sh")[0],
+            script_path=args.script_path,
+            script_name=script_name,
         )
 
-    # print(f"{User.get_script()['script_meta'][0]}")
     # Lists instances
     instances = User.get_instances()
     for instance in instances:
         if instance.name == args.name:
+            print(f"Status: {instance.status}")
             if args.destroy == True:
                 print(f"Destroying {args.name} instance...")
                 instance.destroy()
@@ -87,6 +96,11 @@ if __name__ == "__main__":
 
             if instance.status == "Running":
                 print(f"Connect to {instance.name} with:\n\n\t{instance.ssh_str}\n")
+                exit()
+
+            if instance.status == "Paused":
+                print(f"Resuming {instance.name} instance...")
+                instance.resume()
                 exit()
 
     if args.destroy == True:
@@ -103,7 +117,7 @@ if __name__ == "__main__":
         hdd=args.hdd,
         name=args.name,
         script_id=script_id,
-        is_reserved=args.is_reserved,
+        is_reserved=(not args.is_spot),
     )
     # AI END
 
--- utils/LatentBlending.sh
+++ utils/LatentBlending.sh
@@ -13,3 +13,5 @@ git clone https://github.com/JimothyJohn/latentblending.git && \
     git checkout dev && \
     pip3 install -U pip && \
     pip3 install -r requirements.txt
+
+# wget -P /root/.cache/torch/hub/checkpoints/ https://download.pytorch.org/models/alexnet-owt-7be5be79.pth && wget -P /root/.cache/torch/hub/checkpoints/ https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt && wget -P /root/.cache/huggingface/hub/models--laion--CLIP-ViT-H-14-laion2B-s32B-b79K/snapshots/94a64189c3535c1cb44acfcccd7b0908c1c8eb23/ https://huggingface.co/laion/CLIP-ViT-H-14-laion2B-s32B-b79K/resolve/main/open_clip_pytorch_model.bin
